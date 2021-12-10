7ca69aaa1c222288fe048542d8a0cc348fb45b0e
Maike Rodrigues <maikerodrigues@cialne.com.br>
2021-12-10 11:00:57 -0300

    Validar e autenticar usuario

--- app/src/main/java/com/maike/organizze/activity/LoginActivity.java
+++ app/src/main/java/com/maike/organizze/activity/LoginActivity.java
@@ -1,16 +1,96 @@
 package com.maike.organizze.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 import com.maike.organizze.R;
+import com.maike.organizze.config.ConfiguracaoFirebase;
+import com.maike.organizze.model.Usuario;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText editEmail, editSenha;
+    private Button buttonEntrar;
+
+    private FirebaseAuth autenticacao;
+    private Usuario usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editEmail = findViewById(R.id.editEmail);
+        editSenha = findViewById(R.id.editSenha);
+        buttonEntrar = findViewById(R.id.buttonEntrar);
+
+        buttonEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String textEmail = editEmail.getText().toString();
+                String textSenha = editSenha.getText().toString();
+
+                if (!textEmail.isEmpty()) {
+                    if (!textSenha.isEmpty()) {
+
+                        usuario = new Usuario();
+                        usuario.setEmail(textEmail);
+                        usuario.setSenha(textSenha);
+                        validarLogin(usuario);
+
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Preencha o senha!", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(LoginActivity.this, "Preencha o email!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void validarLogin(Usuario usuario) {
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        autenticacao.signInWithEmailAndPassword(usuario.getEmail(), usuario.getSenha()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    abrirTelaPrincipal();
+                   // Toast.makeText(LoginActivity.this, "Sucesso ao fazer login", Toast.LENGTH_SHORT).show();
+                }else{
+                    String excecao = "";
+                    try {
+                        throw task.getException();
+                    }catch (FirebaseAuthInvalidUserException e) {
+                        excecao = "Usuário não está cadastrado";
+                    }catch (FirebaseAuthInvalidCredentialsException e){
+                        excecao = "E-mail e senha não correspondem a um usuário cadastrado";
+                    }catch (Exception e){
+                        excecao = "Erro ao logar usuário: " + e.getMessage() ;
+                        e.printStackTrace();
+                    }
+
+                    Toast.makeText(LoginActivity.this, excecao, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    public void abrirTelaPrincipal(){
+        startActivity(new Intent(this, PrincipalActivity.class));
+        finish();
     }
 }
