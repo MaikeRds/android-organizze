2a25743f52d65adb547fc73f57b5a6d670ea2b5f
Maike Rodrigues <maikerodrigues@cialne.com.br>
2021-12-10 10:10:25 -0300

    Tratamento de execeções de cadastro

--- app/src/main/java/com/maike/organizze/activity/CadastroActivity.java
+++ app/src/main/java/com/maike/organizze/activity/CadastroActivity.java
@@ -1,16 +1,92 @@
 package com.maike.organizze.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.maike.organizze.R;
+import com.maike.organizze.config.ConfiguracaoFirebase;
+import com.maike.organizze.model.Usuario;
 
 public class CadastroActivity extends AppCompatActivity {
+
+    private EditText editNome, editEmail, editSenha;
+    private Button botaoCadastrar;
+
+    private FirebaseAuth autenticacao;
+    private Usuario usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        editNome = findViewById(R.id.editNome);
+        editEmail = findViewById(R.id.editEmail);
+        editSenha = findViewById(R.id.editSenha);
+
+        botaoCadastrar = findViewById(R.id.buttonCadastrar);
+
+        botaoCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String textNome = editNome.getText().toString();
+                String textEmail = editEmail.getText().toString();
+                String textSenha = editSenha.getText().toString();
+
+                //Validar se os campos foram preenchidos
+
+                if (!textNome.isEmpty()) {
+                    if (!textEmail.isEmpty()) {
+                        if (!textSenha.isEmpty()) {
+
+                            usuario = new Usuario();
+                            usuario.setNome(textNome);
+                            usuario.setEmail(textEmail);
+                            usuario.setSenha(textSenha);
+
+                            cadastrarUsuarios(usuario);
+                        } else {
+                            Toast.makeText(CadastroActivity.this, "Preencha o senha!", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(CadastroActivity.this, "Preencha o email!", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(CadastroActivity.this, "Preencha o nome!", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
+
+    public void cadastrarUsuarios(Usuario usuario){
+
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        autenticacao.createUserWithEmailAndPassword(usuario.getEmail(), usuario.getSenha()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(CadastroActivity.this, "Sucesso ao cadastrar usuário!", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(CadastroActivity.this, "Erro ao cadastrar usuário!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+
 }
--- app/src/main/java/com/maike/organizze/activity/MainActivity.java
+++ app/src/main/java/com/maike/organizze/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.maike.organizze;
+package com.maike.organizze.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -6,8 +6,7 @@ import android.view.View;
 
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
-import com.maike.organizze.activity.CadastroActivity;
-import com.maike.organizze.activity.LoginActivity;
+import com.maike.organizze.R;
 
 public class MainActivity extends IntroActivity {
 
