ebdec6164cebe920c14cd17481059c82ff82c861
Maike Rodrigues <maikerodrigues@cialne.com.br>
2021-12-17 10:41:36 -0300

    Salvando despesa

--- app/src/main/java/com/maike/organizze/activity/DespesasActivity.java
+++ app/src/main/java/com/maike/organizze/activity/DespesasActivity.java
@@ -3,16 +3,21 @@ package com.maike.organizze.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.maike.organizze.R;
 import com.maike.organizze.helper.DateCustom;
+import com.maike.organizze.model.Movimentacao;
+import com.maike.organizze.repository.MovimentacaoRepository;
 
 public class DespesasActivity extends AppCompatActivity {
 
     private TextInputEditText editData, editCategoria, editDescricao;
     private EditText editValor;
+    private Movimentacao movimentacao;
+    private MovimentacaoRepository movimentacaoRepository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +31,18 @@ public class DespesasActivity extends AppCompatActivity {
 
         //Preencher o campo data co a date atual
         editData.setText(DateCustom.dataAtual());
+
+        movimentacaoRepository = new MovimentacaoRepository();
+    }
+
+    public void salvarDespesa(View view) {
+        movimentacao = new Movimentacao();
+        movimentacao.setValor(Double.parseDouble(editValor.getText().toString()));
+        movimentacao.setCategoria(editCategoria.getText().toString());
+        movimentacao.setDescricao(editDescricao.getText().toString());
+        movimentacao.setData(editData.getText().toString());
+        movimentacao.setTipo("D");
+
+        movimentacaoRepository.salvar(movimentacao);
     }
 }
---
+++ app/src/main/java/com/maike/organizze/repository/MovimentacaoRepository.java
@@ -0,0 +1,26 @@
+package com.maike.organizze.repository;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.maike.organizze.config.ConfiguracaoFirebase;
+import com.maike.organizze.helper.Base64Custom;
+import com.maike.organizze.helper.DateCustom;
+import com.maike.organizze.model.Movimentacao;
+import com.maike.organizze.model.Usuario;
+
+public class MovimentacaoRepository {
+    private DatabaseReference databaseReference = ConfiguracaoFirebase.getFirebaseDatabase();
+
+    public void salvar(Movimentacao movimentacao){
+
+        FirebaseAuth auth = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
+        String idUsuario = Base64Custom.codificarBase64(auth.getCurrentUser().getEmail());
+
+        databaseReference.child("movimentacao")
+                .child(idUsuario)
+                .child(DateCustom.mesAnoDataEscolhida(movimentacao.getData()))
+                .push()
+                .setValue(movimentacao);
+    }
+}
