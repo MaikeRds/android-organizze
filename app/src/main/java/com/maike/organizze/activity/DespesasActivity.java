8ef16f6f67ad587cbdf275c6847b817731fa7fc7
Maike Rodrigues <maikerodrigues@cialne.com.br>
2021-12-17 09:06:13 -0300

    Exibindo data atual

--- app/src/main/java/com/maike/organizze/activity/DespesasActivity.java
+++ app/src/main/java/com/maike/organizze/activity/DespesasActivity.java
@@ -3,14 +3,28 @@ package com.maike.organizze.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.maike.organizze.R;
+import com.maike.organizze.helper.DateCustom;
 
 public class DespesasActivity extends AppCompatActivity {
+
+    private TextInputEditText editData, editCategoria, editDescricao;
+    private EditText editValor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_despesas);
+
+        editData      = findViewById(R.id.editData);
+        editCategoria = findViewById(R.id.editCategoria);
+        editDescricao = findViewById(R.id.editDescricao);
+        editValor     = findViewById(R.id.editValor);
+
+        //Preencher o campo data co a date atual
+        editData.setText(DateCustom.dataAtual());
     }
 }
